1265d54724f662807ca1db6cbdc859146942a76d
daslu <daniel.slutsky@gmail.com>
2020-06-08 14:38:01 +0300

    updated app

--- app1/app.R
+++ app1/app.R
@@ -2,11 +2,13 @@ library(shiny)
 
 ui <- fluidPage(
   tags$head(tags$script(src = "message-handler.js")),
+  titlePanel("....")
   actionButton("Pull", "Pull apps from github"))
 
 server = function(input, output, session) {
   observeEvent(input$do, {
-    system("(cd /home/shiny/apps; git pull)")
+    system(touch "/tmp/a")
+    #system("(cd /home/shiny/apps; git pull)")
   })
 }
 
